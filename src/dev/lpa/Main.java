ad444db0a3cfd29c9dbbe2c02b6ccf954e1463ca
adamd <adam.d.hinton@gmail.com>
2024-03-02 10:03:11 -0500

    declare basic fields

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,25 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+}
+
+
+class Bank{
+    private String name;
+    private ArrayList<Branch> branches;
+}
+
+class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+}
+
+class Customer{
+    private String name;
+    private ArrayList<Double> transactions;
 }
