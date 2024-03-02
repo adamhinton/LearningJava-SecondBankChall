fa0337a31c956418dabb3839cd258acb01bc21b0
adamd <adam.d.hinton@gmail.com>
2024-03-02 10:08:51 -0500

    flesh out Customer

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -12,14 +12,50 @@ public class Main {
 class Bank{
     private String name;
     private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
 }
 
 class Branch {
     private String name;
     private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
 }
 
 class Customer{
     private String name;
     private ArrayList<Double> transactions;
+
+    public Customer(String name, Double initialTransaction) {
+        this.name = name;
+        transactions = new ArrayList<Double>();
+        transactions.add(initialTransaction);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void addTransaction(Double transactionAmount){
+        transactions.add(transactionAmount);
+    }
 }
